f78089f4a7c4a58d9af7b9077f358083ac3b7ed8
Daniil <dkappdev@icloud.com>
2022-03-07 14:08:35 +0300

    Update project settings

--- Lingwords/SceneDelegate.swift
+++ Lingwords/SceneDelegate.swift
@@ -11,10 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         let viewController = UIViewController()
         viewController.view.backgroundColor = .systemBackground
+        
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
